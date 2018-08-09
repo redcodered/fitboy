3c13a71e93bb0b0c8bc86d60e963a55a88848011
Shane Kamar <shanekamar@Mac.com>
2018-08-09 06:15:34 -0400

    WIP commit: Added more to the devise scaffolding, about to add Authy and subsequently token-backed authentication for the react client.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'landing/index'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root 'landing#index'
   mount Nutrition::API => '/api/nutrition'
   get '/app(/*all)', to: 'viewer_app#app'
   get '/nutrition/test', to: 'viewer_app#test'
